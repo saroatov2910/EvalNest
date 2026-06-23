77a979b25db7f236f03ea936e13c032ca52f106f
MartellHub <lesnik669@gmail.com>
2026-06-23 20:47:06 +0300

    icon pacage

--- Client/src/app/page.tsx
+++ Client/src/app/page.tsx
@@ -1,23 +1,41 @@
-import Image from 'next/image'
-import Logo from './IMG/Logo.jpeg'
+import Image from 'next/image';
+import Logo from './IMG/Logo.jpeg';
+// icons
+import { RxHamburgerMenu } from "react-icons/rx";
+
 
 export default function Home() {
   return (
     <div>
-        <nav className="bg-[#1B3D2B] text-white p-4">
-        <div className="container mx-auto flex items-center justify-between">
-            <div className="text-lg font-bold">
-                <Image src={Logo} alt="Logo" width={60} height={60} className="inline-block mr-2 rounded-full" />
-                MyApp
-            </div>
-            <div>
-                <a href="#" className="px-3 py-2 hover:bg-blue-700 rounded">Home</a>
-                <a href="#" className="px-3 py-2 hover:bg-blue-700 rounded">About</a>
-                <a href="#" className="px-3 py-2 hover:bg-blue-700 rounded">Contact</a>
-            </div>
+      <nav className='bg-white text-black p-4'>
+        <div className='container mx-auto flex items-center justify-between'>
+          <a
+            href='#'
+            className='px-3 py-2d'
+          >
+            <RxHamburgerMenu size={24} />
+          </a>
+          <div className='text-lg font-bold'>
+            <Image
+              src={Logo}
+              alt='Logo'
+              width={60}
+              height={60}
+              className='inline-block mr-2 rounded-full'
+            />
+            MyApp
+          </div>
+          <div>
+            <a
+              href='#'
+              className='px-3 py-2 '
+            >
+              Sing In
+            </a>
+            
+          </div>
         </div>
-        </nav>
-
+      </nav>
     </div>
-  )
+  );
 }
